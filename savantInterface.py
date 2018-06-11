8004d65d89736792734027658c293dcb7e4fbf13
Dom Maurice <dom@Doms-Mac-mini.local>
2018-06-11 23:31:04 +0100

    Debugging
    Change: Savant interface error variable same as function

--- savantInterface.py
+++ savantInterface.py
@@ -3,7 +3,7 @@ import urllib2
 baseUrl = 'http://dom-home-server.com:12000/servicerequest%20%22Living%20Room%22%20'
 
 #Off
-off = '%22%22%20%22%22%20%22%22%20%22%22%20PowerOff'
+offURL = '%22%22%20%22%22%20%22%22%20%22%22%20PowerOff'
 
 #Chromecast On
 chromecast = 'Chromecast%20%22Video%20Source%22%201%20SVC_AV_HDMI%20PowerOn'
@@ -18,7 +18,7 @@ retropie = 'RetroPie%20%22Video%20Source%22%201%20SVC_AV_HDMI%20PowerOn'
 virgin = '%22Virgin%20Media%20TV%22%20%22Cable%20TV%20Tuner%22%201%20SVC_AV_TV%20PowerOn'
 
 def off():
-    contents = urllib2.urlopen(baseUrl + off).read()
+    contents = urllib2.urlopen(baseUrl + offURL).read()
 
 def chromecastOn():
     contents = urllib2.urlopen(baseUrl + chromecast).read()
